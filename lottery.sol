279caa84ad34948bf162c5624d622ccff09407dd
Michael <444647888@qq.com>
2019-03-01 13:03:19 +0800

    final version

--- lottery.sol
+++ lottery.sol
@@ -4,17 +4,22 @@ contract Lottery {
     address payable manager;
     address payable winner;
     address payable [] lotteryPlayers;
+
     uint public winningNum;
     uint public roundNum;
     uint public rewardRate=80;
     uint public winningReward;
+    uint public lotteryBet=1 ether;
 
+    uint public drawStartTime=now;
+    uint public drawEndTime=now + 30 minutes;
     constructor() public {
         manager = msg.sender;
     }
 
     function throwIn() public payable{
-        require(msg.value == 1 ether);
+        require(msg.value == lotteryBet);
+        require(now < drawStartTime);
         lotteryPlayers.push(msg.sender);
     }
 
@@ -26,6 +31,7 @@ contract Lottery {
     //event test(uint,uint);
     function draw() public managerLimit  {
         require(lotteryPlayers.length != 0);
+        require(now >= drawStartTime && now < drawEndTime);
         bytes memory randomInfo = abi.encodePacked(now,block.difficulty,lotteryPlayers.length);
         bytes32 randomHash =keccak256(randomInfo);
         winningNum = uint(randomHash)%lotteryPlayers.length;
@@ -36,6 +42,8 @@ contract Lottery {
         //emit test(reward,address(this).balance);
         manager.transfer(address(this).balance);
         roundNum++;
+        drawStartTime+=1 days;
+        drawEndTime+=1 days;
         delete lotteryPlayers;
     }
     function getBalance()public view returns(uint){
@@ -46,6 +54,27 @@ contract Lottery {
     }
     function getManager()public view returns(address){
         return manager;
+    }
+
+    function getLotteryPlayers() public view returns(address payable [] memory){
+        return lotteryPlayers;
+    }
+
+    function refund()public managerLimit{
+        require(lotteryPlayers.length != 0);
+        require(now>=drawEndTime);
+        uint lenLotteryPlayers = lotteryPlayers.length;
+        for(uint i = 0; i<lenLotteryPlayers;i++){
+            lotteryPlayers[i].transfer(lotteryBet);
+        }
+        roundNum++;
+        drawStartTime+=1 days;
+        drawEndTime+=1 days;
+        delete lotteryPlayers;
+
+    }
+    function getPlayersNum() public view returns(uint){
+        return lotteryPlayers.length;
     }
 
 }
