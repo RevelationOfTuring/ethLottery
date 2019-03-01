16922a07e2794558846ec7668c92c8d5d9b3cfba
Michael <444647888@qq.com>
2019-03-01 12:21:13 +0800

    draw

--- lottery.sol
+++ lottery.sol
@@ -1,10 +1,13 @@
 pragma solidity^0.5.0;
 
 contract Lottery {
-    address public manager;
-    address public winner;
-    address[] public lotteryPlayers;
+    address payable manager;
+    address payable winner;
+    address payable [] lotteryPlayers;
+    uint public winningNum;
     uint public roundNum;
+    uint public rewardRate=80;
+    uint public winningReward;
 
     constructor() public {
         manager = msg.sender;
@@ -14,4 +17,35 @@ contract Lottery {
         require(msg.value == 1 ether);
         lotteryPlayers.push(msg.sender);
     }
+
+    modifier managerLimit {
+        require(msg.sender == manager);
+        _;
+    }
+
+    //event test(uint,uint);
+    function draw() public managerLimit  {
+        require(lotteryPlayers.length != 0);
+        bytes memory randomInfo = abi.encodePacked(now,block.difficulty,lotteryPlayers.length);
+        bytes32 randomHash =keccak256(randomInfo);
+        winningNum = uint(randomHash)%lotteryPlayers.length;
+        winner=lotteryPlayers[winningNum];
+
+        winningReward = address(this).balance*rewardRate/100;
+        winner.transfer(winningReward);
+        //emit test(reward,address(this).balance);
+        manager.transfer(address(this).balance);
+        roundNum++;
+        delete lotteryPlayers;
+    }
+    function getBalance()public view returns(uint){
+        return address(this).balance;
+    }
+    function getWinner()public view returns(address){
+        return winner;
+    }
+    function getManager()public view returns(address){
+        return manager;
+    }
+
 }
